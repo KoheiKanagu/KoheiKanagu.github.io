712bdf9cca260195a64af10e968f2ac361b70773
KoheiKanagu <KoheiKanagu@users.noreply.github.com>
2020-01-19 15:40:27 +0000

    Automated deployment: Sun Jan 19 15:40:27 UTC 2020 488c7924afd9d442ff530a89bbfe3d0129ce8b8f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,16 +1,19 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
-"/assets/LICENSE": "bb323171ad363741716e1765af1f13f6",
-"/assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
+  "/assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "bdd8d75eb9e6832ccd3117e06c51f0d3",
+"/assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "3ca122272cfac33efb09d0717efde2fa",
+"/assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "d21f791b837673851dd14f7c132ef32e",
+"/assets/AssetManifest.json": "e26ab0f0a9ebee8025260de29a88a4ca",
+"/assets/LICENSE": "ab528034b183ee1fac16085992dba5ed",
+"/assets/FontManifest.json": "5fa2baa1355ee1ffd882bec6ab6780c7",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "/assets/images/nyan.gif": "1a0d759837911e89a318c2266249ba5d",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/manifest.json": "d23be53b9bca0d202829d296313d0af4",
 "/index.html": "4f96ad99a2a5672a27e29408feb0adc2",
-"/main.dart.js": "f03b995a65f24aefe78ba796e5a68e4f"
+"/main.dart.js": "6244eb91fe0774aaa458f29732d9de52"
 };
 
 self.addEventListener('activate', function (event) {
