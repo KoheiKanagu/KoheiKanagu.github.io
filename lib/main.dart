6ce1101c6a54b86caca08b88fef733f000a732a2
KoheiKanagu <kanagu@kingu.dev>
2020-01-25 15:42:07 +0900

    init

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,18 @@ import 'package:firebase/firebase.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  initializeApp(
-    apiKey: "AIzaSyCzeHjFxrux4QRFWXFFmVoITuPplQr8HMA",
-    authDomain: "kingu-dev.firebaseapp.com",
-    databaseURL: "https://kingu-dev.firebaseio.com",
-    projectId: "kingu-dev",
-    storageBucket: "kingu-dev.appspot.com",
-    messagingSenderId: "715119498389",
-    appId: "1:715119498389:web:5407c887b027b68217379c",
-    measurementId: "G-W0FCGYNSME",
-  );
+  if (apps.isEmpty) {
+    initializeApp(
+      apiKey: "AIzaSyCzeHjFxrux4QRFWXFFmVoITuPplQr8HMA",
+      authDomain: "kingu-dev.firebaseapp.com",
+      databaseURL: "https://kingu-dev.firebaseio.com",
+      projectId: "kingu-dev",
+      storageBucket: "kingu-dev.appspot.com",
+      messagingSenderId: "715119498389",
+      appId: "1:715119498389:web:5407c887b027b68217379c",
+      measurementId: "G-W0FCGYNSME",
+    );
+  }
   runApp(MyApp());
 }
 
