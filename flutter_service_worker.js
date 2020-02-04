1299f64b3663cbcf03330b1890e24ceb63e41002
KoheiKanagu <KoheiKanagu@users.noreply.github.com>
2020-02-04 01:33:05 +0000

    Automated deployment: Tue Feb  4 01:33:05 UTC 2020 4f71f71e36654e8b7515a05bb5df477eb983ce56

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,15 +5,15 @@ const RESOURCES = {
 "/assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "3ca122272cfac33efb09d0717efde2fa",
 "/assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "d21f791b837673851dd14f7c132ef32e",
 "/assets/AssetManifest.json": "e26ab0f0a9ebee8025260de29a88a4ca",
-"/assets/LICENSE": "22795e17d6032fb233c311c441969bce",
+"/assets/LICENSE": "f911fadd9f2df5307f9efbdf2a3939aa",
 "/assets/FontManifest.json": "5fa2baa1355ee1ffd882bec6ab6780c7",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "/assets/images/nyan.gif": "1a0d759837911e89a318c2266249ba5d",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/manifest.json": "d23be53b9bca0d202829d296313d0af4",
-"/index.html": "4f96ad99a2a5672a27e29408feb0adc2",
-"/main.dart.js": "59a8af6b345d70b54bb422445455d34f"
+"/index.html": "b24661f23abba51e7de3012a110bc6ae",
+"/main.dart.js": "1baf3c160dbdc812a6520800818adc6e"
 };
 
 self.addEventListener('activate', function (event) {
